a2fdb5995c4fce012c06773a5f1c7f7ec1d1475a
ronnymikalsen <ronny.mikalsen@nasjonalbiblioteket.org>
2021-03-31 10:53:51 +0200

    build: fix release scripts

--- scripts/release/release.sh
+++ scripts/release/release.sh
@@ -12,14 +12,16 @@ cd $(dirname $0)/../..
 git checkout master; git pull origin master
 yarn build:libs
 yarn build:elements
-git add -f dist && standard-version -a
 
 CURRENT_VERSION=$(node -p "require('./package.json').version")
 cd dist/libs/@nationallibraryofnorway/ngx-mime
+cp ../../../../README.md .
 
 echo "Version: $CURRENT_VERSION"
 npm version $CURRENT_VERSION
 
 cd ../../../..
+
+git add -f dist && standard-version -a
 git push --follow-tags origin master
 npm publish dist/libs/@nationallibraryofnorway/ngx-mime
